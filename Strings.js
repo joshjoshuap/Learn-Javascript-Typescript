1f864680e4af2db5edf530a4099eaf53adce1658
Joshua <joshuap16.personal@gmail.com>
2022-01-09 19:03:27 +0800

    learn strings

---
+++ Strings.js
@@ -0,0 +1,42 @@
+// Strings
+// String Index
+let name = 'Joshua';
+console.log(name[0]);
+
+// String Length
+console.log(name.length);
+
+//Index of
+console.log(name.indexOf('s'));
+
+// Slice
+console.log(name.slice(3, 6));
+
+// Slice String Length
+console.log(name.slice(name.lastIndexOf(' ') + 1));
+
+// Get last letter
+console.log(name.slice(-1));
+
+// Upper Case
+console.log(name.toUpperCase());
+
+// Lower Case
+console.log(name.toLowerCase());
+
+// String to Numbers
+let num1 = '1';
+console.log(Number(num1));
+
+// Replace
+let address = 'Quezon City, Philippines';
+let changedAddress  = address.replace('Quezon', 'Manila');
+console.log(changedAddress);
+
+// Includes
+let name = 'Joshu a'
+if (name.includes(' ')) {
+  console.log('Name contained spaces')
+} else {
+  console.log(`Hello ${name}`);
+}
