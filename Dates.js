d1a8b3033bb29dadff8bac3b262b674ea36fcaad
Joshua <joshuap16.personal@gmail.com>
2022-01-11 18:07:42 +0800

    learn dates

---
+++ Dates.js
@@ -0,0 +1,95 @@
+// Dates
+// Basics
+let d = new Date();
+console.log(d);
+
+// Year
+console.log(`Year: ${d.getFullYear()}`);
+
+// Month
+console.log(`Month: ${d.getMonth() + 1} `);
+
+// Date
+console.log(`Date: ${d.getDate()} `);
+
+// Day
+console.log(`Day: ${d.getDay()} `);
+
+// Hours
+console.log(`Hours: ${d.getHours()} `);
+
+// Minutes
+console.log(`Minutes: ${d.getMinutes()} `);
+
+// Seconds
+console.log(`Seconds: ${d.getSeconds()} `);
+
+// Date with Switch Case
+let month = "";
+switch (d.getMonth() + 1) {
+  case 1:
+    month = "January";
+    break;
+  case 2:
+    month = "February";
+    break;
+  case 3:
+    month = "March";
+    break;
+  case 4:
+    month = "April";
+    break;
+  case 5:
+    month = "May";
+    break;
+  case 6:
+    month = "June";
+    break;
+  case 7:
+    month = "July";
+    break;
+  case 8:
+    month = "August";
+    break;
+  case 9:
+    month = "September";
+    break;
+  case 10:
+    month = "October";
+    break;
+  case 11:
+    month = "November";
+    break;
+  case 12:
+    month = "December";
+    break;
+}
+
+let day = "";
+switch (d.getDay()) {
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+    break;
+  case 7:
+    day = "Sunday";
+    break;
+}
+
+console.log(
+  `Today is year ${d.getUTCFullYear()} Month of ${month} and date is ${d.getDate()} which is ${day}`
+);
