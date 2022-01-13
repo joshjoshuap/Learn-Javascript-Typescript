64de141d4ded12c9e839227c12213f21dd91d06c
Joshua <joshuap16.personal@gmail.com>
2022-01-13 15:02:20 +0800

    protoype

--- OOP.js
+++ OOP.js
@@ -1,12 +1,13 @@
 // Object Oriented Programming
 
 // Constructor Function
-function registerUser(userName, userPassword, userEmail, userFirstName, userLastName) {
+function registerUser(userName, userPassword, userEmail, userFirstName, userLastName, userAge) {
   this.userName = userName;
   this.userPassword = userPassword;
   this.userEmail = userEmail;
   this.userFirstName = userFirstName;
   this.userLastName = userLastName;
+  this.userAge = userAge;
 }
 
 let user1 = new registerUser(
@@ -14,10 +15,31 @@ let user1 = new registerUser(
   "Joshu123",
   "josh@email.com",
   "Joshua",
-  "Pautanes"
+  "Pautanes",
+  21
+)
+
+let user2 = new registerUser(
+  "Juan",
+  "000",
+  "juan@email.com",
+  "Juan",
+  "Tamad",
+  1
 )
 
 console.log(
-`Username: ${user1.userName}\nPassword: ${user1.userPassword}\nEmail: ${user1.userEmail}\nFirstname: ${user1.userFirstName}\nLastname: ${user1.userLastName}
+`Username: ${user1.userName}\nPassword: ${user1.userPassword}\nEmail: ${user1.userEmail}\nFirstname: ${user1.userFirstName}\nLastname: ${user1.userLastName}\nAge: ${user1.userAge}
 `);
  
+ // Prototypes
+ registerUser.prototype.checkAge = function() {
+  if (this.userAge >= 18) {
+    return "You are Allowed";
+  } else {
+    return "Minor not Allowed";
+  }
+}
+
+console.log(`Hello ${user1.userFirstName} ${user1.checkAge()}`); // call checkAge prototype
+console.log(`Hello ${user2.userFirstName} ${user2.checkAge()}`);
