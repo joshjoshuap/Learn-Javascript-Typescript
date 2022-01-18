f174de02a9e38fd744391bd09a49a42ffcc16824
Joshua <joshuap16.personal@gmail.com>
2022-01-18 18:03:58 +0800

    resolve reject, chain promises

--- Promises.js
+++ Promises.js
@@ -23,3 +23,38 @@ checkAge(age)
   .catch((err) => {
     console.log(err);
   });
+
+// Promises Resolve - Reject
+let userLogin = (username, password) => {
+  return new Promise((resolve, reject) => {
+    if (username === "admin" && password === "admin") return resolve("Successfull Login");
+    if (username !== "admin" && password === "admin") return reject("Username Invalid");
+    if (username === "admin" && password !== "admin") return reject("Password Invalid");
+    if (username !== "admin" && password !== "admin") return reject("Both username and password Invalid");
+    if (!username && !password) return reject("Missing Credentials");
+  });
+};
+
+userLogin("admin", "admins")
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Chain Promises
+let displayName = (name) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(name);
+      resolve();
+    }, 1000);
+  });
+};
+
+displayName("Joshua")
+  .then(() => displayName("Juan"))
+  .then(() => displayName("Jose"))
+  .then(() => displayName("Maria"))
+  .then(() => displayName("Emilio"));
