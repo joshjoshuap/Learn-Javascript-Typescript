c3fcb20c7b88cfa50be9d248bf994ef6073b8838
Joshua <joshuap16.personal@gmail.com>
2022-01-20 17:03:53 +0800

    add 7d 24h volume

--- Build App/CryptoPrice - API/scripts.js
+++ Build App/CryptoPrice - API/scripts.js
@@ -10,8 +10,8 @@ let cryptoAPI = {
   eth: "https://api.coinlore.net/api/ticker/?id=80",
   xrp: "https://api.coinlore.net/api/ticker/?id=58",
   bnb: "https://api.coinlore.net/api/ticker/?id=2710",
-  usdt: "https://api.coinlore.net/api/ticker/?id=518"
-}
+  usdt: "https://api.coinlore.net/api/ticker/?id=518",
+};
 
 // Fetching Crypto
 fetchCrypto = async (APIurl) => {
@@ -19,9 +19,8 @@ fetchCrypto = async (APIurl) => {
     const res = await fetch(APIurl); // fetching api url
     data = await res.json(); // convert JSON to object
     return data; // return data objects
-    
   } catch (err) {
-    console.log("Fetch Error"); 
+    console.log("Fetch Error");
     console.log(err.message); // fetch failed
   }
 };
@@ -32,7 +31,22 @@ fetchCrypto(cryptoAPI.btc).then((data) => {
     descBTC[0].textContent = coin.name;
     descBTC[1].textContent = coin.symbol;
     descBTC[2].textContent = coin.price_usd;
-  })
+    descBTC[3].textContent = coin.percent_change_7d;
+    descBTC[4].textContent = coin.percent_change_24h;
+    descBTC[5].textContent = coin.volume24;
+
+    if (coin.percent_change_7d >= 0.1) {
+      descBTC[3].style.color = "green";
+    } else {
+      descBTC[3].style.color = "red";
+    }
+
+    if (coin.percent_change_24h >= 0.1) {
+      descBTC[4].style.color = "green";
+    } else {
+      descBTC[4].style.color = "red";
+    }
+  });
 });
 
 // ETH
@@ -41,7 +55,22 @@ fetchCrypto(cryptoAPI.eth).then((data) => {
     descETH[0].textContent = coin.name;
     descETH[1].textContent = coin.symbol;
     descETH[2].textContent = coin.price_usd;
-  })
+    descETH[3].textContent = coin.percent_change_7d;
+    descETH[4].textContent = coin.percent_change_24h;
+    descETH[5].textContent = coin.volume24;
+    
+    if (coin.percent_change_7d >= 0.1) {
+      descETH[3].style.color = "green";
+    } else {
+      descETH[3].style.color = "red";
+    }
+
+    if (coin.percent_change_24h >= 0.1) {
+      descETH[4].style.color = "green";
+    } else {
+      descETH[4].style.color = "red";
+    }
+  });
 });
 
 // XRP
@@ -50,7 +79,22 @@ fetchCrypto(cryptoAPI.xrp).then((data) => {
     descXRP[0].textContent = coin.name;
     descXRP[1].textContent = coin.symbol;
     descXRP[2].textContent = coin.price_usd;
-  })
+    descXRP[3].textContent = coin.percent_change_7d;
+    descXRP[4].textContent = coin.percent_change_24h;
+    descXRP[5].textContent = coin.volume24;
+    
+    if (coin.percent_change_7d >= 0.1) {
+      descXRP[3].style.color = "green";
+    } else {
+      descXRP[3].style.color = "red";
+    }
+
+    if (coin.percent_change_24h >= 0.1) {
+      descXRP[4].style.color = "green";
+    } else {
+      descXRP[4].style.color = "red";
+    }
+  });
 });
 
 // BNB
@@ -59,7 +103,22 @@ fetchCrypto(cryptoAPI.bnb).then((data) => {
     descBNB[0].textContent = coin.name;
     descBNB[1].textContent = coin.symbol;
     descBNB[2].textContent = coin.price_usd;
-  })
+    descBNB[3].textContent = coin.percent_change_7d;
+    descBNB[4].textContent = coin.percent_change_24h;
+    descBNB[5].textContent = coin.volume24;
+    
+    if (coin.percent_change_7d >= 0.1) {
+      descBNB[3].style.color = "green";
+    } else {
+      descBNB[3].style.color = "red";
+    }
+
+    if (coin.percent_change_24h >= 0.1) {
+      descBNB[4].style.color = "green";
+    } else {
+      descBNB[4].style.color = "red";
+    }
+  });
 });
 
 // USDT
@@ -68,9 +127,23 @@ fetchCrypto(cryptoAPI.usdt).then((data) => {
     descUSDT[0].textContent = coin.name;
     descUSDT[1].textContent = coin.symbol;
     descUSDT[2].textContent = coin.price_usd;
-  })
-});
+    descUSDT[3].textContent = coin.percent_change_7d;
+    descUSDT[4].textContent = coin.percent_change_24h;
+    descUSDT[5].textContent = coin.volume24;
 
+    if (coin.percent_change_7d >= 0.1) {
+      descUSDT[3].style.color = "green";
+    } else {
+      descUSDT[3].style.color = "red";
+    }
+
+    if (coin.percent_change_24h >= 0.1) {
+      descUSDT[4].style.color = "green";
+    } else {
+      descUSDT[4].style.color = "red";
+    }
+  });
+});
 
 // Btc - 90
 // eth - 80
