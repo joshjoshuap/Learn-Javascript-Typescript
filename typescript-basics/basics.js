717866d76d0b0b6c5ec974fbfcd747f1b8bd388a
Joshua <joshuap16.personal@gmail.com>
2022-04-20 16:27:02 +0800

    typescript basic

---
+++ typescript-basics/basics.js
@@ -0,0 +1,27 @@
+// Primitives
+var age;
+var wrongAge;
+age = 12;
+wrongAge = '1'; // warning error
+var userName;
+userName = 'Josh';
+var isValid;
+isValid = true;
+// Array
+var names; // string array
+names = ['Josh', 'John'];
+var numbers; // number array
+numbers = [1, 2, 3];
+// Objects
+var person; // object with data type
+person = {
+    name: 'Josh',
+    age: 21
+};
+person = {
+    name: 'Josh',
+    age: '21'
+};
+var section; // union type
+section = '1';
+section = 1;
